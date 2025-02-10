a067c83def2da176bc76e0f5e0dc6f041202d8d7
Doan Danh Du <22520260@gm.uit.edu.vn>
2025-02-10 16:17:43 +0700

    add global exception handler

---
+++ book-network/src/main/java/com/demo05/book_network/handler/ExceptionResponse.java
@@ -0,0 +1,26 @@
+package com.demo05.book_network.handler;
+
+import java.util.Map;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ExceptionResponse {
+
+    private Integer businessErrorCode;
+    private String businessErrorDescription;
+    private String error;
+    private Set<String> validationErrors;
+    private Map<String, String> errors;
+
+}
