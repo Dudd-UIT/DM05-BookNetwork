efe8aed15db15b721047d8a3c5e84d4164c6f5aa
Doan Danh Du <22520260@gm.uit.edu.vn>
2025-02-17 20:30:29 +0700

    add ci/cd pipeline

--- book-network-ui/src/app/app.config.ts
+++ book-network-ui/src/app/app.config.ts
@@ -16,10 +16,11 @@ import {
   withInterceptors,
 } from '@angular/common/http';
 import { httpTokenInterceptor } from './services/interceptors/http-token.interceptor';
+import { ApiModule } from './services/api.module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(CodeInputModule),
+    importProvidersFrom(CodeInputModule, ApiModule.forRoot()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
